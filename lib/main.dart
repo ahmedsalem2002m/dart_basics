20a566303890257089dee77243a434712a21449c
Ahmed Salem <ahmedsalem2002m@gmail.com>
2026-06-25 10:33:09 +0300

    intro variable

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+main() {
+  String name = "Ali";
+  print("My name is $name");
+  print("My name is $name");
+  print("My name is $name");
+  print("My name is $name");
+  print("My name is $name");
+  print("My name is $name");
+  print("My name is $name");
+  print("My name is $name");
+  print("My name is $name");
+  print("My name is $name");
+}
