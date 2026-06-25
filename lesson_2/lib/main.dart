576c1118fe6a33cbe27bca227df6b130a22d56b1
Ahmed Salem <ahmedsalem2002m@gmail.com>
2026-06-25 11:13:32 +0300

    String data type

---
+++ lesson_2/lib/main.dart
@@ -0,0 +1,14 @@
+void main() {
+  String message = 'My name\'s is Ahmed';
+  print(message);
+   message = 'My name\n is Ahmed';
+  print(message);
+   message = 'My name\t is Ahmed';
+  print(message);
+   message = r'My name\n is Ahmed';
+  print(message);
+  message = r'price is 15$';
+  print(message);
+
+
+}
