bb152462a067b18f391e2c31ca89448fe05dd87e
Ahmed Salem <ahmedsalem2002m@gmail.com>
2026-06-26 23:33:53 +0300

    Arithmetic  operators

---
+++ lesson_9/lib/main.dart
@@ -0,0 +1,18 @@
+void main() {
+  int num1 = 18;
+  int num2 = 10;
+
+  int result1 = num1 + num2;
+  int result2 = num1 - num2;
+  int result3 = num1 * num2;
+  double result4 = num1 / num2;
+  int result5 = num1 ~/ num2;
+  int result6 = num1 % num2;
+
+  print(result1);
+  print(result2);
+  print(result3);
+  print(result4);
+  print(result5);
+  print(result6);
+}
