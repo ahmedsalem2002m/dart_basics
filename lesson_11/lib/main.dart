c6595ffed6b0fb5a4de71f77b26f13ac075f444f
Ahmed Salem <ahmedsalem2002m@gmail.com>
2026-06-27 10:30:08 +0300

    List

---
+++ lesson_11/lib/main.dart
@@ -0,0 +1,14 @@
+void main() {
+  List<String> studentsName = ["Ahmed", "Ali", "Ali", "Sameh"];
+
+  print(studentsName);
+  print(studentsName[0]);
+  print(studentsName[2]);
+  print(studentsName[3]);
+  print(studentsName.length);
+  print(studentsName.reversed);
+  print(studentsName.toSet());
+  print(studentsName.removeLast());
+  print(studentsName.removeAt(0));
+  
+}
