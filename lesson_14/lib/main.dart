b70af1facc78bac11384cc9d77febf1dee73bb4b
Ahmed Salem <ahmedsalem2002m@gmail.com>
2026-06-27 11:31:59 +0300

    Comparsion oerator

---
+++ lesson_14/lib/main.dart
@@ -0,0 +1,12 @@
+void main() {
+  int mathScore = 80;
+  int arbScore = 85;
+
+  bool isAraSmaller = arbScore < mathScore;
+  bool isAraEqual = arbScore == mathScore;
+  bool isAraHigher = arbScore > mathScore;
+
+  print(isAraSmaller);
+  print(isAraHigher);
+  print(isAraEqual);
+}
