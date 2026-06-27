5af861d0201c0f33574098e2d494685797dbc304
Ahmed Salem <ahmedsalem2002m@gmail.com>
2026-06-27 11:27:28 +0300

    Map

---
+++ lesson_13/lib/main.dart
@@ -0,0 +1,9 @@
+void main() {
+  Map<String, num> ages = {"Ahmed": 12, "Ali": 15, "Omar": 11, "Sameh": 13};
+
+  print("Age of Ahmed is ${ages["Ahmed"]}");
+
+  Map<int, int> numberOfSeats = {1: 120, 3: 40, 4: 20, 6: 5};
+
+  print("Age of room 5 = ${numberOfSeats[5]}");
+}
