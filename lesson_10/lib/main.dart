e6de3b226c8442ba7e7b5bf4de0b93a4fa3c94c0
Ahmed Salem <ahmedsalem2002m@gmail.com>
2026-06-27 10:10:47 +0300

    dot operator

---
+++ lesson_10/lib/main.dart
@@ -0,0 +1,20 @@
+void main() {
+  String name = "Ahmed";
+  print(name.isNotEmpty);
+  print(name.length);
+  print(name.compareTo("Ahmed"));
+  print(name.toUpperCase());
+  print(name.trim());
+  print(name.indexOf("d"));
+
+  print("-------------------------------------");
+
+  int age = 22;
+  print(age.bitLength);
+  print(age.isOdd);
+  print(age.toString());
+  print(age.toDouble());
+  print(age.compareTo(23));
+
+  
+}
