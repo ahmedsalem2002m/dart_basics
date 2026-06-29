291bf3e57931a76ea7142c1aee4705e34d27ac61
Ahmed Salem <ahmedsalem2002m@gmail.com>
2026-06-29 09:12:04 +0300

    If condition

---
+++ lesson_15/lib/main.dart
@@ -0,0 +1,14 @@
+void main() {
+  bool weatherStatus = true;
+
+  if (weatherStatus) {
+    print("Go to sea");
+  }
+
+  int mathScore = 30;
+  bool hasHePassed = mathScore >= 50;
+
+  if (hasHePassed) {
+    print("Congratulation you passed");
+  }
+}
